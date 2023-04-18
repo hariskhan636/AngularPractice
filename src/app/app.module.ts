959f156e03b249ae8d636d6dafb7fd3e431dfd2e
haris.khan636 <harisjriv@gmail.com>
2023-04-18 03:46:38 -0700

    Session3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,   //When AppModule loads, its components load eagerly
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, 
+    RouterModule.forRoot([
+      {
+        path:'login',
+        component: LoginComponent  //components are not standalone but have to be part of a module
+      },
+      {
+        path:'signup',
+        component: SignupComponent
+      },
+    ]
+    )
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
